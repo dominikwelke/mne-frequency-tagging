7aef1f21b40d22ee4ac02246cb18b8941119b617
dwelke <dominik.welke@ae.mpg.de>
2020-11-03 17:00:53 +0100

    update __init__.py

--- mne_frequency_tagging/__init__.py
+++ mne_frequency_tagging/__init__.py
@@ -1,5 +1,7 @@
 from .frequency_tagging import snr_spectrum
 from .frequency_tagging import snr_at_frequency
-from .frequency_tagging import plot_snr
+from .frequency_tagging import plot_psd_spectrum
+from .frequency_tagging import plot_snr_spectrum
 from .frequency_tagging import plot_snr_topography
+from .frequency_tagging import FtSpectra
 
